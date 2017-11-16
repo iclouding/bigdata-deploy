c3fdcbc69d67433bdd02c418b0ee7eacc6957497
连凯 <lian.kai@whaley.cn>
2017-11-16 17:39:44 +0800

    空body的校验失败码改为-1 更新nginx.hosts文件 日志收集主机初始化时下发nginx配置文件

--- modules/nginx/config/ngx-extsvr-log-v2.0/online/nginx_init.lua
+++ modules/nginx/config/ngx-extsvr-log-v2.0/online/nginx_init.lua
@@ -110,7 +110,7 @@ end
 
 
 --消息体签名验证
---@return -2:空请求体；-1:签名校验失败； 0：没有做签名校验； 1：签名校验成功
+--@return -1:签名校验失败； 0：没有做签名校验； 1：签名校验成功
 function doMsgSign()
     local headers = ngx.req.get_headers()
     local logSignMethod = headers["log-sign-method"]
