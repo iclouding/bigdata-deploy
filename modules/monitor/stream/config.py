7bea5aa42e74392df3a6831fff90a8801d8311c3
pengtao <peng.tao@whaley.cn>
2017-11-21 16:21:48 +0800

    stream监控脚本下发

--- modules/monitor/stream/config.py
+++ modules/monitor/stream/config.py
@@ -12,7 +12,13 @@ streaming = [{'hosts': 'bigdata-appsvr-130-3', 'type': 'yarn_list', 'keyword': '
              'users': 'spark',
              'commands': 'sh /opt/ai/interestOptimization-201707/release/bin/online-long-videos.sh',
              'alter_mail': True},
+             {'hosts': 'bigdata-appsvr-130-3', 'type': 'ps_keyword', 'keyword': 'MoreTV_FrontPagePersonal',
+              'users': 'spark', 'commands': 'sh /opt/ai/kafkaIO/shell/MoreTV_InterestOptimizationOffline.sh',
+              'alter_mail': True},
              {'hosts': 'bigdata-appsvr-130-3', 'type': 'ps_keyword', 'keyword': 'MoreTV_FrontPagePersonalOnline',
               'users': 'spark', 'commands': 'sh/opt/ai/kafkaIO/shell/MoreTV_InterestOptimizationOnline.sh',
+              'alter_mail': True},
+             {'hosts': 'bigdata-appsvr-130-3', 'type': 'ps_keyword', 'keyword': 'MoreTV_FrontPageOnlineSidGeneration',
+              'users': 'spark', 'commands': 'sh /opt/ai/kafkaIO/shell/MoreTV_InterestOnlineSidGeneration.sh',
               'alter_mail': True}
             ]
