dfe26d4e36833f836d17b38547efd16e2f2a0312
pengtao <peng.tao@whaley.cn>
2017-11-22 20:01:24 +0800

    stream监控脚本fix error

--- modules/monitor/stream/monitor_stream_couchbase.py
+++ modules/monitor/stream/monitor_stream_couchbase.py
@@ -116,7 +116,7 @@ class Checktask():
 
     def check_ps_keyword_workflow(self):
         log_msg("check_ps", "keyword %s,type %s " % (self.keyword, self.type), 1)
-        if not self.check_ps_keyword_service():
+        if not my_check_ps_keyword_service(self.keyword):
             start_service = "su - {0} -c '{1}'".format(self.users, self.commands)
             sub = "{0} {1} 服务不存在".format(socket.gethostname(), self.keyword)
             found_no_service = "{0} 服务并不存在，启动脚本开启服务".format(self.keyword)
@@ -124,7 +124,7 @@ class Checktask():
             send_alter_mail(sub, found_no_service)
             run_command_out(start_service)
             time.sleep(60)
-            if not self.check_ps_keyword_service():
+            if not my_check_ps_keyword_service(self.keyword):
                 sub = "{0} {1}服务重启未成功".format(socket.gethostname(), self.keyword)
                 start_failed = "{0}服务运行脚本开启服务失败".format(self.keyword)
                 log_msg("start", start_failed, 2)
@@ -139,6 +139,16 @@ class Checktask():
             return True
         else:
             return False
+
+
+def my_check_ps_keyword_service(keyword):
+    check_cmd = " ".join(["ps","-aux","|","grep","-i '{0}'","|","grep -v grep"]).format(keyword)
+    import pdb;pdb.set_trace()
+    output = run_command_out(check_cmd)
+    if output:
+        return True
+    else:
+        return False
 
 
 class myThread(threading.Thread):
