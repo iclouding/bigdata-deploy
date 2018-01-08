3451dad5eba18a9f3b728096026694bf79a72993
wang.baozhi <286790238@qq.com>
2018-01-08 20:55:23 +0800

    测试环境-滚动升级

---
+++ modules/hadoop/scripts/rolling_test_hbase_write.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+for ((i=1; i<=100000; i ++))
+do
+    filename="hbase_import"
+    echo "put 'check_hadoop_rolling_write','${i}','count','${i}'" > ${filename}
+    sleep 2s
+    /opt/hbase/bin/hbase shell ${filename}
+done
+
+#hadoop@bigtest-appsvr-129-1
+# /home/hadoop/check_script
+# nohup sh rolling_test_hbase_write.sh >hbase.log 2>&1 &
+
+# create 'check_hadoop_rolling_write','count'
+# put 'check_hadoop_rolling_write','0','count','0'
+# scan 'check_hadoop_rolling_write'
+
+
+#check Database integrity
+#count 'check_hadoop_rolling_write'
