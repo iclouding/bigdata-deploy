361d456c7cfc3fc2cc36b30e37d4053427f67629
wang.baozhi <286790238@qq.com>
2018-02-08 09:47:37 +0800

    logstash6.11 for app 8,9,used for monitor

--- modules/filebeat/config/start_filebeat.sh
+++ modules/filebeat/config/start_filebeat.sh
@@ -15,7 +15,7 @@ else
   echo "config_name is ${config_name}!"
 fi
 
-pro_num=`ps -ef|grep "$config_name"|grep -v grep|grep -v "$0"|wc -l`
+pro_num=`ps -ef|grep -e '/filebeat'|grep "$config_name"|grep -v grep|grep -v "$0"|wc -l`
 echo "pro_num is $pro_num"
 if [ ${pro_num} -gt 0 ]; then
   echo "filebeat is already exist,no need to restart"
